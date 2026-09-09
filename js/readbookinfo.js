47655416c3a92d09ecbda9a9b42ae90981cf6813
hongdonjoo70 <hongdonjoo1970.gmail.com>
2026-09-09 14:59:48 +0900

    Update indentation

--- js/readbookinfo.js
+++ js/readbookinfo.js
@@ -1,332 +1,332 @@
-        const bestsellerTitle = [
-            "세네카, 오늘을 빼앗기고 있는 당신에게",
-            "신경 끄기의 기술 (한교동 에디션)" ,
-            "아멜리아의 호기심 많은 " ,
-            "오딧세이아" ,
-            "찌니주의보" ,
-            "그랬다고 적었다" ,
-            "싯다르타" ,
-            "기쁨에도 슬픔에도 아랑곳..." ,
-            "흔한남매 과학 탐험대 18" ,
-            "우런니 샌드위치"
-        ];
+const bestsellerTitle = [
+    "세네카, 오늘을 빼앗기고 있는 당신에게",
+    "신경 끄기의 기술 (한교동 에디션)",
+    "아멜리아의 호기심 많은 ",
+    "오딧세이아",
+    "찌니주의보",
+    "그랬다고 적었다",
+    "싯다르타",
+    "기쁨에도 슬픔에도 아랑곳...",
+    "흔한남매 과학 탐험대 18",
+    "우런니 샌드위치"
+];
 
-        const editorChoiceTitle = [
-            "극한 진화의 세계",
-            "살 것인가, 팔 것인가, 버틸 것인가 Buy·Sell·Hold",
-            "슬로 호시스",
-            "엄마가 도망갔다",
-            "당신과 내가 대화할 수 있을까",
-            "서리꽃",
-            "레몬나무의 바다",
-            "동구와 친구들 : 가을 알밤을 찾아라",
-            "자유민주주의에 무슨 일이 일어났는가?",
-            "ETF 투자 5일 완성",
-            "오디세이 각본집",
-            "칵테일을 마시는 철학자들",
-            "그랬다고 적었다",
-            "에티의 여름",
-            "언어화를 위한 소설 사고",
-            "코끼리 고모부",
-            "그 길 위에 우리가 있었다"
-        ];
+const editorChoiceTitle = [
+    "극한 진화의 세계",
+    "살 것인가, 팔 것인가, 버틸 것인가 Buy·Sell·Hold",
+    "슬로 호시스",
+    "엄마가 도망갔다",
+    "당신과 내가 대화할 수 있을까",
+    "서리꽃",
+    "레몬나무의 바다",
+    "동구와 친구들 : 가을 알밤을 찾아라",
+    "자유민주주의에 무슨 일이 일어났는가?",
+    "ETF 투자 5일 완성",
+    "오디세이 각본집",
+    "칵테일을 마시는 철학자들",
+    "그랬다고 적었다",
+    "에티의 여름",
+    "언어화를 위한 소설 사고",
+    "코끼리 고모부",
+    "그 길 위에 우리가 있었다"
+];
 
-        // 2. 실시간 클릭 Top 10 (트렌딩 도서)
-        const trendingTitle = [
-            "손끝까지 숨이 차면",
-            "노무현 평전",
-            "찌니주의보",
-            "오뒷세이아",
-            "살 것인가, 팔 것인가, 버틸 것인가 Buy·Sell·Hold",
-            "오디세이아 (국내 유일 명화 104점 수록 완역본)",
-            "칵테일을 마시는 철학자들",
-            "타이거! 타이거!",
-            "엔시티피케이션",
-            "엄마가 도망갔다"
-        ];
+// 2. 실시간 클릭 Top 10 (트렌딩 도서)
+const trendingTitle = [
+    "손끝까지 숨이 차면",
+    "노무현 평전",
+    "찌니주의보",
+    "오뒷세이아",
+    "살 것인가, 팔 것인가, 버틸 것인가 Buy·Sell·Hold",
+    "오디세이아 (국내 유일 명화 104점 수록 완역본)",
+    "칵테일을 마시는 철학자들",
+    "타이거! 타이거!",
+    "엔시티피케이션",
+    "엄마가 도망갔다"
+];
 
-        // 3. 다시 뽑아보는 3대 미스터리
-        const mysteryTitle = [
-            "13.67",
-            "용의자 X의 헌신",
-            "그리고 아무도 없었다",
-            "점성술 살인사건",
-            "화차",
-            "스밀라의 눈에 대한 감각",
-            "양들의 침묵",
-            "샤이닝",
-            "태양은 가득히",
-            "빅 픽처",
-            "나오미와 가나코",
-            "상복의 랑데부"
-        ];
+// 3. 다시 뽑아보는 3대 미스터리
+const mysteryTitle = [
+    "13.67",
+    "용의자 X의 헌신",
+    "그리고 아무도 없었다",
+    "점성술 살인사건",
+    "화차",
+    "스밀라의 눈에 대한 감각",
+    "양들의 침묵",
+    "샤이닝",
+    "태양은 가득히",
+    "빅 픽처",
+    "나오미와 가나코",
+    "상복의 랑데부"
+];
 
-        // 4. 만권당 MD의 선택
-        const mdChoiceTitle = [
-            "주린이가 가장 알고 싶은 최다질문 TOP 77",
-            "AI 2026 트렌드&활용백과",
-            "평균의 종말",
-            "하우스콘서트에 진심",
-            "감정을 마주하면 길이 보인다",
-            "열 세가지 수수께끼",
-            "하루 15분 호르몬 혁명",
-            "24세 정신과 영수증"
-        ];
+// 4. 만권당 MD의 선택
+const mdChoiceTitle = [
+    "주린이가 가장 알고 싶은 최다질문 TOP 77",
+    "AI 2026 트렌드&활용백과",
+    "평균의 종말",
+    "하우스콘서트에 진심",
+    "감정을 마주하면 길이 보인다",
+    "열 세가지 수수께끼",
+    "하루 15분 호르몬 혁명",
+    "24세 정신과 영수증"
+];
 
-        // 5. 이달의 주목도서
-        const notableTitle = [
-            "만희네 꽃밭",
-            "봄 여름 가을 겨울, 시와 함께",
-            "레몬은 시다",
-            "고래의 노래",
-            "토끼 씨의 완벽한 가방",
-            "2120",
-            "손끝까지 숨이 차면",
-            "극한 진화의 세계",
-            "슬기로운 외국어 생활",
-            "전략적 방임 교육"
-        ];
+// 5. 이달의 주목도서
+const notableTitle = [
+    "만희네 꽃밭",
+    "봄 여름 가을 겨울, 시와 함께",
+    "레몬은 시다",
+    "고래의 노래",
+    "토끼 씨의 완벽한 가방",
+    "2120",
+    "손끝까지 숨이 차면",
+    "극한 진화의 세계",
+    "슬기로운 외국어 생활",
+    "전략적 방임 교육"
+];
 
-        // 6. 이달의 특가
-        const specialPriceTitle = [
-            "불변의 법칙",
-            "100억 젊은 부자들이 온다",
-            "트렌드 코리아 2026",
-            "마흔에 읽는 쇼펜하우어",
-            "탐라순력도 1702년, 제주를 돌아보다",
-            "일러스트로 보는 영국 건축의 언어",
-            "기막힌 SNS 수학",
-            "베니스 건축 스케치북",
-            "수리수리봉봉 마법사",
-            "꿈은 토리노를 달리고"
-        ];
+// 6. 이달의 특가
+const specialPriceTitle = [
+    "불변의 법칙",
+    "100억 젊은 부자들이 온다",
+    "트렌드 코리아 2026",
+    "마흔에 읽는 쇼펜하우어",
+    "탐라순력도 1702년, 제주를 돌아보다",
+    "일러스트로 보는 영국 건축의 언어",
+    "기막힌 SNS 수학",
+    "베니스 건축 스케치북",
+    "수리수리봉봉 마법사",
+    "꿈은 토리노를 달리고"
+];
 
-        // 7. 알라디너의 선택
-        const aladinerChoiceTitle = [
-            "우리가 빛의 속도로 갈 수 없다면",
-            "깨끗한 존경",
-            "시간의 감촉",
-            "에티의 여름",
-            "그림자를 이으면 길이 된다",
-            "노무현 평전",
-            "신경 끄기의 기술",
-            "기쁨에서 슬품에도 아랑곳없이",
-            "브레인 악셀",
-            "백야",
-            "당신과 내가 대화할 수 있을까",
-            "문해내공"
+// 7. 알라디너의 선택
+const aladinerChoiceTitle = [
+    "우리가 빛의 속도로 갈 수 없다면",
+    "깨끗한 존경",
+    "시간의 감촉",
+    "에티의 여름",
+    "그림자를 이으면 길이 된다",
+    "노무현 평전",
+    "신경 끄기의 기술",
+    "기쁨에서 슬품에도 아랑곳없이",
+    "브레인 악셀",
+    "백야",
+    "당신과 내가 대화할 수 있을까",
+    "문해내공"
 
-        ];
+];
 
-        const bookOfTheWeekTitle = [
-            "사람, 장소, 환대",
-            "우리가 빛의 속도로 갈 수 없다면",
-            "아픔이 길이 되려면",
-            "작별하지 않는다",
-            "전쟁은 여자의 얼굴을 하지 않았다",
-            "실격당한 자들을 위한 변론",
-            "한국 신자유주의의 기원과 형성",
-            "밝은 밤"
-        ];
+const bookOfTheWeekTitle = [
+    "사람, 장소, 환대",
+    "우리가 빛의 속도로 갈 수 없다면",
+    "아픔이 길이 되려면",
+    "작별하지 않는다",
+    "전쟁은 여자의 얼굴을 하지 않았다",
+    "실격당한 자들을 위한 변론",
+    "한국 신자유주의의 기원과 형성",
+    "밝은 밤"
+];
 
-        // 2. 신간소개 / 새로나온책 (New Releases)
-        const newBookTitle = [
-            "센바 쿠로노의 강해지는 마작",
-            "믿음이 넘어진 자리",
-            "여름을 한 입 베어 물었더니",
-            "신 퇴마록 신세편 1",
-            "단다단 21",
-            "코리아타운의 핫 칙",
-            "샤먼 서쪽 숲에 갔다",
-            "보호 관찰"
-        ];
+// 2. 신간소개 / 새로나온책 (New Releases)
+const newBookTitle = [
+    "센바 쿠로노의 강해지는 마작",
+    "믿음이 넘어진 자리",
+    "여름을 한 입 베어 물었더니",
+    "신 퇴마록 신세편 1",
+    "단다단 21",
+    "코리아타운의 핫 칙",
+    "샤먼 서쪽 숲에 갔다",
+    "보호 관찰"
+];
 
-        // 3. 화제의 책 / 화제의 신작 (Trending/Hot Issue Books)
-        const hotIssueTitle = [
-            "하늘의 눈",
-            "한국사 이상현상 연구원",
-            "신세계의 아이들",
-            "파괴된 남자",
-            "단편들, 한국 공포 문학의 밤",
-            "아르고스",
-            "타이거! 타이거!",
-            "그렉 이건 더 베스트",
-            "수수께끼 풀이는 저녁식사 후에 1",
-            "매스커레이드 라이프"
-        ];
+// 3. 화제의 책 / 화제의 신작 (Trending/Hot Issue Books)
+const hotIssueTitle = [
+    "하늘의 눈",
+    "한국사 이상현상 연구원",
+    "신세계의 아이들",
+    "파괴된 남자",
+    "단편들, 한국 공포 문학의 밤",
+    "아르고스",
+    "타이거! 타이거!",
+    "그렉 이건 더 베스트",
+    "수수께끼 풀이는 저녁식사 후에 1",
+    "매스커레이드 라이프"
+];
 
-        // 4. 눈에 띄는 새책 (Noticeable New Books)
-        const noticeableNewTitle = [
-            "위스퍼맨",
-            "서성이다",
-            "이상한 지도",
-            "응답의 시",
-            "친절한 유괴범",
-            "측백나무성의 라푼젤",
-            "호신술 클럽",
-            "엔딩을 통과하는 중"
-        ];
+// 4. 눈에 띄는 새책 (Noticeable New Books)
+const noticeableNewTitle = [
+    "위스퍼맨",
+    "서성이다",
+    "이상한 지도",
+    "응답의 시",
+    "친절한 유괴범",
+    "측백나무성의 라푼젤",
+    "호신술 클럽",
+    "엔딩을 통과하는 중"
+];
 
-        const eventGoodsTitle = [
-            "오디세이아 (국내 유일 명화 104점 수록 완역본)",
-            "태양 아래 올리브",
-            "투명한 나선",
-            "지푸라기 왕관을 쓴 여자",
-            "복숭아와 애벌레",
-            "급류",
-            "일리아스 & 오디세이아 세트",
-            "더 시스템"
-        ];
+const eventGoodsTitle = [
+    "오디세이아 (국내 유일 명화 104점 수록 완역본)",
+    "태양 아래 올리브",
+    "투명한 나선",
+    "지푸라기 왕관을 쓴 여자",
+    "복숭아와 애벌레",
+    "급류",
+    "일리아스 & 오디세이아 세트",
+    "더 시스템"
+];
 
-        /* 
-        참고: 만약 '책 제목'이 아니라 '굿즈 이름' 자체가 텍스트로 필요하시다면 
-        아래 배열을 활용해 보세요.
-        const goodsName = [
-            "격자무늬 원통형 가방", 
-            "책 키링 카드지갑", 
-            "피너츠 미니 클립보드", 
-            "치이카와 쿨담요", 
-            "일월오봉도 냥냥 봉투",
-            "명화 금박 마스킹 테이프"
-        ];
-        */
+/* 
+참고: 만약 '책 제목'이 아니라 '굿즈 이름' 자체가 텍스트로 필요하시다면 
+아래 배열을 활용해 보세요.
+const goodsName = [
+    "격자무늬 원통형 가방", 
+    "책 키링 카드지갑", 
+    "피너츠 미니 클립보드", 
+    "치이카와 쿨담요", 
+    "일월오봉도 냥냥 봉투",
+    "명화 금박 마스킹 테이프"
+];
+*/
 
-        // 2. 지금 펀딩중 (알라딘 북펀드 도서)
-        // 출간 전 독자들의 펀딩을 받고 있는 프로젝트 도서들입니다.
-        const bookFundTitle = [
-            "까마귀 덫",
-            "장벽 너머",
-            "토리빵 1~7권 세트",
-            "우리가 몰랐던 세계의 도서관",
-            "상투적으로 불행하고, 특별하게 행복한",
-            "고양이 탐정단의 비밀 (양장본)",
-            "모든 것의 시작: 우주의 기원",
-            "잊혀진 여성 독립운동가 열전"
-        ];
+// 2. 지금 펀딩중 (알라딘 북펀드 도서)
+// 출간 전 독자들의 펀딩을 받고 있는 프로젝트 도서들입니다.
+const bookFundTitle = [
+    "까마귀 덫",
+    "장벽 너머",
+    "토리빵 1~7권 세트",
+    "우리가 몰랐던 세계의 도서관",
+    "상투적으로 불행하고, 특별하게 행복한",
+    "고양이 탐정단의 비밀 (양장본)",
+    "모든 것의 시작: 우주의 기원",
+    "잊혀진 여성 독립운동가 열전"
+];
 
-        const ebookTitles = [
-          "그랬다고 적었다",
-          "자유민주주의에 무슨 일이 일어났는가?",
-          "어떻게 살아낼 것인가",
-          "에티의 여름",
-          "피를 마시는 새 오디오북",
-          "언어화를 위한 소설 사고",
-          "찌니주의보",
-          "당신의 독자가 될게요",
-          "새들의 사회성",
-          "우리가 사랑한 책",
-          "안녕, 피터팬",
-          "투명한 나선",
-          "인비인",
-          "책 읽고 싶어서 회사를 그만뒀습니다"
-        ];
-        const foreignBookTitles = [
-          "Theo of Golden",
-          "Spider-Man: Brand New Day - The Art of the Movie",
-          "Dragon Masters #32 : Heart of the Ruby Dragon",
-          "ペイジェムウィ-クリ- コンパクト-i メモ日曜 パ-ルブル",
-          "The Odyssey: The Official Screenplay",
-          "Under the Oak Tree: Volume 3",
-          "Paw Patrol Dino Rescue My Busy Books",
-          "spoon.2Di vol.136",
-          "Inner Excellence: Train Your Mind for Extraordinary Performance and the Best Possible Life",
-          "The Odyssey",
-          "Magic School Bus : Science Readers Box ",
-          "CanCam(キャンキャン) 2026年 10月號",
-          "Disney/Pixar the Art of Toy Story 5",
-          "Poor Charlie's Almanack: The Essential Wit and Wisdom of Charles T. Munger",
-          "Disney/Pixar Toy Story 5: The Junior Novelization",
-          "Casa BRUTUS(カ-サ ブル-タス) 2026年 9月號"
-        ];
-        const specialBookTitles = [
-          "삼체",
-          "욕구들",
-          "우리는 모두 죽는다는 것을 기억하라",
-          "내가 되는 꿈",
-          "문장강화",
-          "예루살렘의 아이히만",
-          "우리가 열 번을 나고 죽을 때",
-          "경애의 마음",
-          "앤디 위어 우주",
-          "낙하",
-        ];
+const ebookTitles = [
+    "그랬다고 적었다",
+    "자유민주주의에 무슨 일이 일어났는가?",
+    "어떻게 살아낼 것인가",
+    "에티의 여름",
+    "피를 마시는 새 오디오북",
+    "언어화를 위한 소설 사고",
+    "찌니주의보",
+    "당신의 독자가 될게요",
+    "새들의 사회성",
+    "우리가 사랑한 책",
+    "안녕, 피터팬",
+    "투명한 나선",
+    "인비인",
+    "책 읽고 싶어서 회사를 그만뒀습니다"
+];
+const foreignBookTitles = [
+    "Theo of Golden",
+    "Spider-Man: Brand New Day - The Art of the Movie",
+    "Dragon Masters #32 : Heart of the Ruby Dragon",
+    "ペイジェムウィ-クリ- コンパクト-i メモ日曜 パ-ルブル",
+    "The Odyssey: The Official Screenplay",
+    "Under the Oak Tree: Volume 3",
+    "Paw Patrol Dino Rescue My Busy Books",
+    "spoon.2Di vol.136",
+    "Inner Excellence: Train Your Mind for Extraordinary Performance and the Best Possible Life",
+    "The Odyssey",
+    "Magic School Bus : Science Readers Box ",
+    "CanCam(キャンキャン) 2026年 10月號",
+    "Disney/Pixar the Art of Toy Story 5",
+    "Poor Charlie's Almanack: The Essential Wit and Wisdom of Charles T. Munger",
+    "Disney/Pixar Toy Story 5: The Junior Novelization",
+    "Casa BRUTUS(カ-サ ブル-タス) 2026年 9月號"
+];
+const specialBookTitles = [
+    "삼체",
+    "욕구들",
+    "우리는 모두 죽는다는 것을 기억하라",
+    "내가 되는 꿈",
+    "문장강화",
+    "예루살렘의 아이히만",
+    "우리가 열 번을 나고 죽을 때",
+    "경애의 마음",
+    "앤디 위어 우주",
+    "낙하",
+];
 
-        let bestsellersBooks = [];
-        let editorChoiceBooks = [];
-        let editorChoiceBooks2 = [];
-        let trendingBooks = [];
-        let mysteryBooks = [];
-        let mdChoiceBooks = [];
-        let notableBooks = [];
-        let specialPriceBooks = [];
-        let aladinerChoiceBooks = [];
-        let bookOfTheWeekBooks = [];
-        let newBookBooks = [];
-        let hotIssueBooks = [];
-        let hotIssueBooks2 = [];
-        let noticeableNewBooks = [];
-        let eventGoodsBooks = [];
-        let bookFundBooks = [];
-        let ebookBooks = [];
-        let foreignBooks = [];
-        let specialBooks = [];
+let bestsellersBooks = [];
+let editorChoiceBooks = [];
+let editorChoiceBooks2 = [];
+let trendingBooks = [];
+let mysteryBooks = [];
+let mdChoiceBooks = [];
+let notableBooks = [];
+let specialPriceBooks = [];
+let aladinerChoiceBooks = [];
+let bookOfTheWeekBooks = [];
+let newBookBooks = [];
+let hotIssueBooks = [];
+let hotIssueBooks2 = [];
+let noticeableNewBooks = [];
+let eventGoodsBooks = [];
+let bookFundBooks = [];
+let ebookBooks = [];
+let foreignBooks = [];
+let specialBooks = [];
 
-        async function fetchBooks(booktitle) {
-            const params = new URLSearchParams({
-                target: "title",
-                query:booktitle,
-                size: 1
-            });
-            const url = `https://dapi.kakao.com/v3/search/book?${params}`;
+async function fetchBooks(booktitle) {
+    const params = new URLSearchParams({
+        target: "title",
+        query: booktitle,
+        size: 1
+    });
+    const url = `https://dapi.kakao.com/v3/search/book?${params}`;
 
-            const response = await fetch(url, {
-                method: 'GET',
-                headers: {
-                    Authorization: "KakaoAK xfa669dbee43bf25c0ad9d49c6126a5c6"
-                }
-            });
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            Authorization: "KakaoAK xfa669dbee43bf25c0ad9d49c6126a5c6"
+        }
+    });
 
-            if (!response.ok) {
-                pass;
+    if (!response.ok) {
+        pass;
+    }
+
+    return response.json();
+}
+
+async function bookData(queries, result) {
+    try {
+        // query와 section ID를 매핑
+        //output.innerHTML += queries.length;
+        for (const index in queries) {
+            const data = await fetchBooks(queries[index]);
+            if (data.documents.length === 0) {
+                // 해당 섹션 내의 .box 요소 1개 선택    
+                result.push({
+                    thumbnail: "./img/book/test" + (index + 1) + ".jpg",
+                    title: queries[index],
+                    authors: "ccc",
+                    contents: "ddd"
+                });
+                continue;
             }
 
-            return response.json();
+            // 해당 섹션 내의 .box 요소 1개 선택    
+            result.push({
+                thumbnail: data.documents[0].thumbnail,
+                title: data.documents[0].title,
+                authors: data.documents[0].authors,
+                contents: data.documents[0].contents
+            });
+        }
+    } catch (error) {
+        for (let index2 = 0; index2 < queries.length; index2++) {
+            result.push({
+                thumbnail: "./img/book/test" + (index2 + 1) + ".jpg",
+                title: queries[index2],
+                authors: "aaa",
+                contents: "bbb"
+            });
         }
 
-        async function bookData(queries,result){
-            try {
-                // query와 section ID를 매핑
-                //output.innerHTML += queries.length;
-                for (const index in queries) {
-                    const data = await fetchBooks(queries[index]);
-                    if(data.documents.length === 0) {
-                    // 해당 섹션 내의 .box 요소 1개 선택    
-                        result.push ({
-                            thumbnail: "./img/book/test"+(index+1)+".jpg",
-                            title: queries[index],
-                            authors: "ccc",
-                            contents: "ddd"
-                        });
-                        continue;
-                    }
-
-                    // 해당 섹션 내의 .box 요소 1개 선택    
-                    result.push ({
-                        thumbnail: data.documents[0].thumbnail,
-                        title: data.documents[0].title,
-                        authors: data.documents[0].authors,
-                        contents: data.documents[0].contents
-                    });
-                }
-            } catch (error) {
-                for (let index2=0; index2<queries.length; index2++) {
-                    result.push ({
-                        thumbnail: "./img/book/test"+(index2+1)+".jpg",
-                        title: queries[index2],
-                        authors: "aaa",
-                        contents: "bbb"
-                    });
-                }
-                
-            }
-        }
+    }
+}
